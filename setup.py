67f6c4b53136252e8fea347050406e361260f6c4
Joshua Hamilton <hamiltonjoshuadavid@gmail.com>
2024-04-26 12:41:35 -0500

    transition from old setuptools.py to modern pyproject.toml

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-from setuptools import setup, find_packages
+import setuptools
 
-with open("README.md", "r") as f:
-    long_description = f.read()
-
-setup(
-    name='srt-to-vtt',
-    version='1.0.0',
-    packages=find_packages(),
-    install_requires=[],
-    author='Joshua Hamilton',
-    author_email='hamiltonjoshuadavid@gmail.com',
-    description='Python package to enable easy conversion of .srt files to .vtt files.',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license='MIT',
-    keywords=['subtitle', 'subtitles', 'convert', 'srt', 'vtt', 'webvtt'],
-    url='https://github.com/joshdavham/srt-to-vtt',
-)
+setuptools.setup()
