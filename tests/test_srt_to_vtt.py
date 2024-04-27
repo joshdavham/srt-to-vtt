7302b13bb35d720c22b724a0ab59b178618a64ac
Joshua Hamilton <hamiltonjoshuadavid@gmail.com>
2024-04-27 13:16:21 -0500

    black existing python code to pass checks

--- tests/test_srt_to_vtt.py
+++ tests/test_srt_to_vtt.py
@@ -1,17 +1,18 @@
 import srt_to_vtt
 
+
 def test_convert():
     """
     Takes test_input.srt, converts it to test_output.vtt using srt_to_vtt.srt_to_vtt
     and compares it with valid_output.vtt.
     """
 
-    srt_to_vtt.srt_to_vtt('test_input.srt', 'test_output.vtt')
+    srt_to_vtt.srt_to_vtt("test_input.srt", "test_output.vtt")
 
-    with open('test_output.vtt', 'r') as vtt_file:
+    with open("test_output.vtt", "r") as vtt_file:
         test_lines = vtt_file.readlines()
 
-    with open('valid_output.vtt', 'r') as vtt_file:
+    with open("valid_output.vtt", "r") as vtt_file:
         valid_lines = vtt_file.readlines()
 
     assert len(test_lines) == len(valid_lines)
@@ -22,5 +23,6 @@ def test_convert():
         valid_line = valid_lines[i]
 
         assert test_line == valid_line
-    
+
+
 test_convert()
